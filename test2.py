77e168e562173583c3f64092bd9b4dbef25ed786
shegde17-we45 <shreyas.hegde@we45.com>
2026-04-21 11:02:45 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,4 +1,12 @@
-import os
+from flask import request
+import sqlite3
 
-def run_command(user_input):
-    os.system("ls " + user_input)   # vulnerable
+def login():
+    conn = sqlite3.connect("app.db")
+
+    username = request.form["user"]
+    password = request.form["pass"]
+
+    query = f"SELECT * FROM users WHERE user='{username}' AND pass='{password}'"
+
+    conn.execute(query)
